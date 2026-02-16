7888a36f5f8a0827f42893a54903b681612eb5ea
Eduardo Morais <emdemor415@gmail.com>
2026-02-16 00:07:37 -0300

    Update build process and add ensure_style script for JupyterLab theme

--- scripts/prepare_labextension.sh
+++ scripts/prepare_labextension.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 mkdir -p jupyterlab_trillia_theme/labextension
 rm -rf jupyterlab_trillia_theme/labextension/*
-cp package.json jupyterlab_trillia_theme/labextension/
-cp -r style jupyterlab_trillia_theme/labextension/
-cp -r lib jupyterlab_trillia_theme/labextension/
+
+# Use the build output produced by `jupyter labextension build .`
+cp static/package.json jupyterlab_trillia_theme/labextension/
+cp -r static/static jupyterlab_trillia_theme/labextension/
+cp -r static/themes jupyterlab_trillia_theme/labextension/
